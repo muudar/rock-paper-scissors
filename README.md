d51b4cf0fb493a929f9e393cb6ca4505e42d912b
Mudhar <mohammedmudher@hotmail.com>
2021-12-20 18:21:15 +0200

    added UI , images and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rock-paper-scissors
+
+# Copyright Ⓒ
+
+Images are from:
+
+https://toppng.com/show_download/164739/kartinka-old-paper-vintage-paper-paper-background-convite-pergaminho-branca-de-neve/large
+
+https://toppng.com/show_download/8951/rock/large
+
+https://toppng.com/show_download/191357/free-scissors-and-glue-clipart-school-scissors-clipart/large
--- script.js
+++ script.js
@@ -15,45 +15,74 @@ function playRound(playerSelection, computerSelection){
     computerSelection = capitalFirstLetter(computerSelection.toLowerCase());
     if(playerSelection == computerSelection)
         return 0;
-    if((playerSelection == "Scissors" && computerSelection == "Paper" ) || (playerSelection == "Paper" && computerSelection == "Rock" ) || (playerSelection == "Paper" && computerSelection == "Rock"))
+    if((playerSelection == "Scissors" && computerSelection == "Paper" ) || (playerSelection == "Paper" && computerSelection == "Rock" ) || (playerSelection == "Rock" && computerSelection == "Scissors"))
         return 1;
     return 2;
 }
 
-function game(){
-    let player = 0;
-    let computer = 0;
-    for(let i = 0; i < 5; i++){
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("Enter play (Rock, Paper, Scissors): ");
-        while(playerSelection.toUpperCase() != "ROCK" && playerSelection.toUpperCase() != "PAPER" && playerSelection.toUpperCase() != "SCISSORS"){
-            console.log("Error! Please enter valid input: ");
-            playerSelection = prompt("Enter play (Rock, Paper, Scissors): ");
-        }
-        let res = playRound(playerSelection, computerSelection);
-        if(res == 1)
-        {
-            console.log("You win! " + playerSelection + " Beats " + computerSelection + ".");
-            player++;
-        }
-        else if (res == 2)
-        {
-            computer++;
-            console.log("You lose! " + computerSelection + " Beats " + playerSelection+ ".");
-        }
-        else{
-            console.log("Draw! You both picked " + playerSelection + ".")
-        }
+let human = 0;
+let computer = 0;
+let gameOver = false;
+
+
+const tryAgainButton = document.querySelector('#tryAgainButton');
+const h = document.querySelector("#human-score");
+const c = document.querySelector("#computer-score");
+const gameWinnerMessage = document.querySelector('#gameWinnerMessage');
+function updateScore(){
+    h.textContent = human;
+    c.textContent = computer;
+    let winner = "";
+    if(human == 5 || computer == 5){
+        if(human == 5)
+            winner = "You are ";
+        else
+            winner = "Computer is ";
+        gameWinnerMessage.textContent = winner + "the winner of the game!";
+        tryAgainButton.style.visibility = "visible"; 
+        gameOver = true;
     }
-    console.log("Player: " + player);
-    console.log("Computer: " + computer);
-    if(player > computer)
-        console.log("You win!");
-    else if(player < computer){
-        console.log("Computer wins!");
-    }
-    else
-        console.log('Draw!');
 }
 
-game()
+updateScore();
+
+const container = document.querySelector('#container');
+const btns = container.querySelectorAll('button');
+const message = document.querySelector("#message");
+const playerPick = document.querySelector('#playerPick');
+const computerPick = document.querySelector('#computerPick');
+btns.forEach(btn => btn.onclick =  (e)=>{
+    if(gameOver)
+    {
+        alert("You must press try again!");
+        return;
+    }
+    let playerSelection = e.target.dataset.key;
+    let computerSelection = computerPlay();
+    let res = playRound(playerSelection, computerSelection);
+    playerPick.src = `${playerSelection}.png`;
+    computerPick.src = `${computerSelection}.png`;
+    if(res == 1)
+    {
+        message.textContent = `You won! Computer played ${computerSelection}`;
+        human++;
+    }
+    else if(res == 2)
+    {
+        message.textContent = `You lost! Computer played ${computerSelection}`;
+        computer++;
+    }
+    else
+        message.textContent = `Draw! You both picked ${computerSelection}`;
+    updateScore();
+});
+
+tryAgainButton.addEventListener("click", function reset(){
+    gameOver = false;
+    human = 0;
+    computer = 0;
+    updateScore();
+    gameWinnerMessage.textContent = "";
+    message.textContent = "";
+    tryAgainButton.style.visibility = "hidden";
+});
